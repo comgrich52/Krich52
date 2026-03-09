5c1ccecbbd23a6af78066afc9af7ffaad2a0118b
comgrich52 <comgrich52@gmail.com>
2026-03-09 10:38:24 +0700

    Modify Vite config to remove tailwindcss
    Removed tailwindcss plugin and updated base path.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
+export default defineConfig({
+    plugins: [react()],
+   base: '/Krich52/',
+})
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
